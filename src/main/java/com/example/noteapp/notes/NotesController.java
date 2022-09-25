2d79a2a883268ebcab894df91674b9a02fd1b312
ahan-stark <ahanabubakar@gmail.com>
2022-09-25 23:34:07 +0530

    initial commit

--- src/main/java/com/example/noteapp/notes/NotesController.java
+++ src/main/java/com/example/noteapp/notes/NotesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrgin
+@CrossOrigin
 public class NotesController {
     NotesService notesService;
 
